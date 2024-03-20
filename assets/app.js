51534d64614d70c4548aea104e437fe7d6a1c642
Harika-BRS <153584946+Harika-BRS@users.noreply.github.com>
2024-03-20 11:11:41 +0530

    files uploaded

--- assets/app.js
+++ assets/app.js
@@ -18,16 +18,25 @@ var longestConsecutive = function(nums) {
 };
 
 document.getElementById("submit").addEventListener("click", function() {
+  calculateLongestSequence();
+});
+
+document.getElementById("intervalsInput").addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    calculateLongestSequence();
+  }
+});
+
+function calculateLongestSequence() {
   const input = document.getElementById("intervalsInput").value;
   const nums = JSON.parse(input);
   const longestSequence = longestConsecutive(nums);
   document.getElementById("output").innerText =
     "Length of Longest Consecutive Sequence: " + longestSequence;
+}
+
+// Add this script to clear the input field on page load
+document.addEventListener('DOMContentLoaded', function() {
+  var intervalsInput = document.getElementById('intervalsInput');
+  intervalsInput.value = ''; // Set the input value to an empty string
 });
-
-
-  // Add this script to clear the input field on page load
-  document.addEventListener('DOMContentLoaded', function() {
-    var intervalsInput = document.getElementById('intervalsInput');
-    intervalsInput.value = ''; // Set the input value to an empty string
-  });
